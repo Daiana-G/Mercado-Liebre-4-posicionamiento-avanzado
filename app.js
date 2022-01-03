f67d5c963b5a75e33daed4b7cb6738415483c7ac
Daiana <Daiana.g.97@hotmail.com>
2022-01-02 23:47:33 -0300

    ML-posicionamiento avanzado

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require ('express')
+const app = express ();
+const port = 3000
+const path = require ('path')
+
+/*Middleware*/
+app.use(express.static('public'))
+
+/*rutas*/
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+})
+
+
+app.listen(port, () =>{
+    console.log(`puerto corriendo en ${port}\n http://localhost:${port}`)
+})
